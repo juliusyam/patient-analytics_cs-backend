79e0fc2b1792603ee544ea940b1806af1640eca2
juliusyam <juliusworldwide@gmail.com>
2024-01-09 13:00:24 +0000

    Removing unused ModelBuilder bits

--- PracticeApplication/Models/Context.cs
+++ PracticeApplication/Models/Context.cs
@@ -1,7 +1,7 @@
 namespace PracticeApplication.Models;
 using Microsoft.EntityFrameworkCore;
 
-public partial class Context : DbContext
+public class Context : DbContext
 {
     public Context(DbContextOptions<Context> options) : base(options)
     {
@@ -21,22 +21,11 @@ public partial class Context : DbContext
         modelBuilder.Entity<User>(user =>
         {
             user.HasKey(e => e.Id);
-            user.Property(e => e.Id).HasColumnType("INT");
-            user.Property(e => e.DateOfBirth).HasColumnType("VARCHAR");
-            user.Property(e => e.Gender).HasColumnType("VARCHAR");
-            user.Property(e => e.Email).HasColumnType("VARCHAR");
-            user.Property(e => e.Address).HasColumnType("VARCHAR");
-            user.Property(e => e.DateCreated).HasColumnType("VARCHAR");
-            user.Property(e => e.DateEdited).HasColumnType("VARCHAR");
         });
 
         modelBuilder.Entity<Patient>(patient =>
         {
             patient.HasKey(e => e.Id);
         });
-        
-        OnModelCreatingPartial(modelBuilder);
     }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
