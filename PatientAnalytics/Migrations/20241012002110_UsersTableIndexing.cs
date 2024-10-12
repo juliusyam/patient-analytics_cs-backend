7c25c57e6f1cd49006b08d63ae212b4d031e05f0
juliusyam <juliusworldwide@gmail.com>
2024-10-12 01:24:05 +0100

    Provide updated database migration for Users Table with indexing

---
+++ PatientAnalytics/Migrations/20241012002110_UsersTableIndexing.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatientAnalytics.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersTableIndexing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username_Email",
+                table: "Users",
+                columns: new[] { "Username", "Email" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Username_Email",
+                table: "Users");
+        }
+    }
+}
